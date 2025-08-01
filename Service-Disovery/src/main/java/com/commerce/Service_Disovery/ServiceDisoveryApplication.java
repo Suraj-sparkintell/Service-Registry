e865ef7a514463e0ced17aa7dd1f3045982f6caf
skandel1 <suraj.kandel@sparkintell.com>
2025-08-01 08:51:34 -0500

    created service discovery

---
+++ Service-Disovery/src/main/java/com/commerce/Service_Disovery/ServiceDisoveryApplication.java
@@ -0,0 +1,15 @@
+package com.commerce.Service_Disovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDisoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDisoveryApplication.class, args);
+	}
+
+}
---
+++ Service-Disovery/src/test/java/com/commerce/Service_Disovery/ServiceDisoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.Service_Disovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDisoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
